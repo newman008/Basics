b6558f68ca929bfb6ee5cc7122239d40e2e7006a
newman008 <yangyangwu008@gmail.com>
2015-09-22 16:52:41 -0400

    Update _101_SymmetricTree.java

--- CodingChallenge/LeetCode/_101_SymmetricTree.java
+++ CodingChallenge/LeetCode/_101_SymmetricTree.java
@@ -11,45 +11,45 @@ class TreeNode {
 
 public class _101_SymmetricTree {
 
-    // recursive
+    	// recursive
 	public boolean isSymmetricR(TreeNode root) {
-        if(root == null) return true;
-        return isMirror(root.left, root.right);
-    }
+		if(root == null) return true;
+		return isMirror(root.left, root.right);
+	}
     
-    public boolean isMirror(TreeNode left, TreeNode right) {
-        if(left == null && right == null) return true;
-        if(left == null || right == null) return false;
-        if(left.val == right.val) {
-            return isMirror(left.left, right.right) && isMirror(left.right, right.left);
-        } else {
-            return false;
-        }
-    }
+	public boolean isMirror(TreeNode left, TreeNode right) {
+		if(left == null && right == null) return true;
+		if(left == null || right == null) return false;
+		if(left.val == right.val) {
+		    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+		} else {
+		    return false;
+		}
+	}
     
-    // iterative
-    public boolean isSymmetric(TreeNode root) {
-        if(root == null) return true;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root.left);
-        stack.push(root.right);
-        
-        while(!stack.isEmpty()) {
-            TreeNode p1 = stack.pop();
-            TreeNode p2 = stack.pop();
-            
-            if(p1 == null && p2 == null) continue;
-            if(p1 == null || p2 == null) return false;
-            if(p1.val == p2.val) {
-                stack.push(p1.left);
-                stack.push(p2.right);
-                stack.push(p1.right);
-                stack.push(p2.left);
-            } else {
-                return false;
-            }
-        }
-        
-        return true;
-    }
+	// iterative
+	public boolean isSymmetric(TreeNode root) {
+		if(root == null) return true;
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root.left);
+		stack.push(root.right);
+		
+		while(!stack.isEmpty()) {
+			TreeNode p1 = stack.pop();
+			TreeNode p2 = stack.pop();
+			
+			if(p1 == null && p2 == null) continue;
+			if(p1 == null || p2 == null) return false;
+			if(p1.val == p2.val) {
+				stack.push(p1.left);
+				stack.push(p2.right);
+				stack.push(p1.right);
+				stack.push(p2.left);
+			} else {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
