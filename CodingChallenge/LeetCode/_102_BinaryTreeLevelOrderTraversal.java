2a3c0f25967f8a722785c491571cb7b76ffc9f64
newman008 <yangyangwu008@gmail.com>
2015-09-22 17:08:04 -0400

    Update _102_BinaryTreeLevelOrderTraversal.java

--- CodingChallenge/LeetCode/_102_BinaryTreeLevelOrderTraversal.java
+++ CodingChallenge/LeetCode/_102_BinaryTreeLevelOrderTraversal.java
@@ -15,26 +15,25 @@ class TreeNode {
 public class _102_BinaryTreeLevelOrderTraversal {
 	
 	public List<List<Integer>> levelOrder(TreeNode root) {
-	        List<List<Integer>> results = new ArrayList<List<Integer>>();
-	        if(root == null) return results;
-	        
-	        Queue<TreeNode> q = new LinkedList<TreeNode>();
-	        q.offer(root);
-	        List<Integer> result = new ArrayList<Integer>();
-	        
-	        while(!q.isEmpty()) {
-	            
-	            Queue<TreeNode> next = new LinkedList<TreeNode>();
-	            for(TreeNode t : q) {
-	                result.add(t.val);
-	                if(t.left != null) next.offer(t.left);
-	                if(t.right != null) next.offer(t.right);
-	            }
-	            results.add(result);
-	            result = new ArrayList<Integer>();
-	            q = new LinkedList<TreeNode>(next);
-	        }
-	        
-	        return results;
-	    }
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+		if(root == null) return results;
+		
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		q.offer(root);
+		List<Integer> result = new ArrayList<Integer>();
+		
+		while(!q.isEmpty()) {
+			Queue<TreeNode> next = new LinkedList<TreeNode>();
+			for(TreeNode t : q) {
+				result.add(t.val);
+				if(t.left != null) next.offer(t.left);
+				if(t.right != null) next.offer(t.right);
+			}
+			results.add(result);
+			result = new ArrayList<Integer>();
+			q = new LinkedList<TreeNode>(next);
+		}
+		
+		return results;
+	}
 }
