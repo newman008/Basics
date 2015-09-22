196573c908496fcc679ec800768b68d471932753
newman008 <yangyangwu008@gmail.com>
2015-09-22 17:29:21 -0400

    Update _201_BitwiseAndOfNumberRange.java

--- CodingChallenge/LeetCode/200-299/_201_BitwiseAndOfNumberRange.java
+++ CodingChallenge/LeetCode/200-299/_201_BitwiseAndOfNumberRange.java
@@ -2,13 +2,13 @@ package LeetCode2;
 
 public class _201_BitwiseAndOfNumberRange {
 	public int rangeBitwiseAnd(int m, int n) {
-        if(m == 0) return 0;
-        int step = 0;
-        while(m != n) {
-            m = m >> 1;
-            n = n >> 1;
-            step++;
-        }
-        return n << step;
-    }
+		if(m == 0) return 0;
+		int step = 0;
+		while(m != n) {
+			m = m >> 1;
+			n = n >> 1;
+			step++;
+		}
+		return n << step;
+	}
 }
