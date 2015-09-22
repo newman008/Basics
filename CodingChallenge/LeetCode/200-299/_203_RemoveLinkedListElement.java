99712bdb0ddbc37dee46ae9597c614d5db4744e1
newman008 <yangyangwu008@gmail.com>
2015-09-22 17:33:45 -0400

    Update _203_RemoveLinkedListElement.java

--- CodingChallenge/LeetCode/200-299/_203_RemoveLinkedListElement.java
+++ CodingChallenge/LeetCode/200-299/_203_RemoveLinkedListElement.java
@@ -9,23 +9,23 @@ class ListNode {
 public class _203_RemoveLinkedListElement {
 	
 	public ListNode removeElements(ListNode head, int val) {
-	if(head == null) return null;
-	
-	ListNode fakeHead = new ListNode(0);
-	fakeHead.next = head;
-	ListNode pre = fakeHead;
-	ListNode cur = head;
-	while(cur != null) {
-		if(cur.val == val) {
-			pre.next = cur.next;
-			cur = cur.next;
-			continue;
-		} else {
-			pre = pre.next;
-			cur = cur.next;
+		if(head == null) return null;
+		
+		ListNode fakeHead = new ListNode(0);
+		fakeHead.next = head;
+		ListNode pre = fakeHead;
+		ListNode cur = head;
+		while(cur != null) {
+			if(cur.val == val) {
+				pre.next = cur.next;
+				cur = cur.next;
+				continue;
+			} else {
+				pre = pre.next;
+				cur = cur.next;
+			}
 		}
+		
+		return fakeHead.next;
 	}
-	
-	return fakeHead.next;
-    }
 }
