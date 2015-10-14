eb176c58d8ac6325a09b551ac894fca446568468
newman1 <yangyangwu008@gmail.com>
2015-10-14 11:27:52 +0800

    update _200_NumberOfIslands.java

--- CodingChallenge/LeetCode/200-299/_200_NumberOfIslands.java
+++ CodingChallenge/LeetCode/200-299/_200_NumberOfIslands.java
@@ -7,7 +7,7 @@ public class _200_NumberOfIslands {
 		int n = grid[0].length;
 		boolean[][] visited = new boolean[m][n];
 		int count = 0;
-	        
+
 		for(int i = 0; i < m; i++) {
 			for(int j = 0; j < n; j++) {
 				if(grid[i][j] == '1' && !visited[i][j]) {
@@ -16,26 +16,26 @@ public class _200_NumberOfIslands {
 				}
 			}
 		}
+
 		return count;
 	}
-    
+
 	private void bfs(char[][] grid, boolean[][] visited, int i, int j) {
-		if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
-			return;
+		if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') return;
 		visited[i][j] = true;
 		bfs(grid, visited, i-1, j);
 		bfs(grid, visited, i+1, j);
 		bfs(grid, visited, i, j-1);
 		bfs(grid, visited, i, j+1);
 	}
-    
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		_200_NumberOfIslands t = new _200_NumberOfIslands();
 		char[][] grid = new char[1][2];
 		grid[0][0] = '1';
 		grid[0][1] = '1';
-		
+
 		System.out.print(t.numIslands(grid));
 	}
 }
